8c2f0cc351c8d58bbc8333030df2fe70be8c66cb
DebasisSaikia <debasishsaikia136@gmail.com>
2024-01-27 17:59:04 +0530

    resolvers added

--- course-1/index.js
+++ course-1/index.js
@@ -13,11 +13,26 @@
 //GRAPHQL SERVER
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import db from "./_db.js";
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games: () => {
+      return db.games;
+    },
+    reviews: () => {
+      return db.reviews;
+    },
+    authors: () => {
+      return db.authors;
+    },
+  }
+};
 
 const server = new ApolloServer({
   typeDefs,
-  
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
