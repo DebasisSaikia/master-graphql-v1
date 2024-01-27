f03b95cdb3ba05189e74588e82d3a2ea38498d2d
DebasisSaikia <debasishsaikia136@gmail.com>
2024-01-27 14:54:23 +0530

    write async

--- course-1/file.js
+++ course-1/file.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 
 //write file(synchronously)
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+// fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
 
+//read file(asynchronously)
+fs.writeFile('notes.txt', 'This file was created by Node.js Async!', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
