a5005917e877d0c0d906fef3279bfbb5db34ce5f
DebasisSaikia <debasishsaikia136@gmail.com>
2024-01-28 21:47:23 +0530

    search integrated

--- client-react/src/components/DisplayData.jsx
+++ client-react/src/components/DisplayData.jsx
@@ -1,4 +1,4 @@
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useLazyQuery } from '@apollo/client';
 import { useState } from 'react';
 import Card from './Card';
 
@@ -21,20 +21,37 @@ query GetMovies{
 }
 `
 
+const GET_SINGLE_MOVIE = gql`
+    query GetMovie($name: String!){
+    findMovie(name: $name) {
+        name
+        yearOfPublication
+    }
+}
+`
+
+
 const DisplayData = () => {
     const {data, loading, error} =useQuery(QUERY_USER, {fetchPolicy: "network-only"})
     const {data:movies} =useQuery(GET_MOVIES, {fetchPolicy: "network-only"})
+    const [fetchMovies, {data:movieData, error:movieError}] = useLazyQuery(GET_SINGLE_MOVIE)
     const [search, setSearch] = useState('')
     if(loading) return <h1>Loading...</h1>
-    if(error) return <h1>Error</h1>
+    if(error || movieError) return <h1>Error</h1>
+    console.log({movieData})
   return (
     <>
     <div className='search-container'>
+    <div >
         <input type="text" placeholder="Search"
          className='search-input' 
          value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            onChange={(e) => {
+                setSearch(e.target.value)
+            }}
          />
+    </div>
+    <button onClick={()=>fetchMovies({variables:{name:search}})}>Fetch</button>
     </div>
     <div className='card-container'>
       {data && data?.users?.map((user) => {
